4ed76a5631749f4248b0997c938784c4c194af43
zhenyiTang <ztang11@illinois.edu>
2023-02-18 16:57:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
