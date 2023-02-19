b48e45077bde8bb2829a7a16e435d8adfbbe7f48
zhenyiTang <ztang11@illinois.edu>
2023-02-18 17:27:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import random
 # today = ny_time.strftime('%Y-%m-%d %H:%M)
 
 # nowtime = datetime.utcnow() - timedelta(hours=5)  # New york time
-nowtime = datetime.utcnow()
+nowtime = datetime.now()
 today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d") #今天的日期
 today = datetime.now()
 
