4f328c554b7f5178515f00f70dbd2395add433b0
zhenyiTang <ztang11@illinois.edu>
2023-05-11 19:58:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def get_words():
   #     return get_words()
   #   return words.json()['data']['text']
   words = requests.get("https://api.shadiao.pro/chp")
-  print(words.json())
-  return words.json()
+  print(words.json()['data']['text'])
+  return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
