85a16c06c4f2f2c7bee1c6cff904a144b1af3005
zhenyiTang <ztang11@illinois.edu>
2023-05-11 19:46:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -34,11 +34,15 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+url = 'https://api.shadiao.pro/chp'
+res = requests.get(url)
+print(res.json())
+
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+#   if words.status_code != 200:
+#     return get_words()
+  return words.json()
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
